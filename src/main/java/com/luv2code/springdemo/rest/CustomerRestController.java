fbe24e74882235dfa3a76770090072f6ca003bcf
Sławomir Lasik <--global>
2020-06-17 15:16:40 +0200

    Added updating existing customer mapping in REST Controller

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -52,4 +52,13 @@ public class CustomerRestController {
         return theCustomer;
     }
 
+    // add mapping for PUT /customers - update existing customer
+    @PutMapping("/customers")
+    public Customer updateCustomer(@RequestBody Customer theCustomer){
+
+        customerService.saveCustomer(theCustomer);
+
+        return theCustomer;
+    }
+
 }
