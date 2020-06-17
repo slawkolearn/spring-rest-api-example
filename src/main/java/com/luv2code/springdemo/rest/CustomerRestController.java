5dbf704348c9f49bc2b217fa282b0b1089b5ef00
Sławomir Lasik <--global>
2020-06-17 13:14:45 +0200

    Added @GetMapping for /api/customers/{customerId} API

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,13 @@ public class CustomerRestController {
     @GetMapping("/customers")
     public List<Customer> getCustomers(){
         return customerService.getCustomers();
+    }
+
+    // add mapping for GET /customers/{customerId}
+
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId){
+        return customerService.getCustomer(customerId);
     }
 
 }
