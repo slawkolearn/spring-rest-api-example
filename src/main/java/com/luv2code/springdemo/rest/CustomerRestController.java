a8839c3bcf1f81d51d351fd104c3411f19128e95
Sławomir Lasik <--global>
2020-06-17 12:52:24 +0200

    Added @RestController and @RequestMapping(/api) in CustomerRestControleer class annotations

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -1,4 +1,9 @@
 package com.luv2code.springdemo.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class CustomerRestController {
 }
