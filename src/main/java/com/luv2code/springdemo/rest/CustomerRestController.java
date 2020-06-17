e40fc3ec80d47a2b31cae2a7e29b3ba68cd50c55
Sławomir Lasik <--global>
2020-06-17 12:53:54 +0200

    Autowiring the customerService in CustomerRestController class

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -1,9 +1,18 @@
 package com.luv2code.springdemo.rest;
 
+import com.luv2code.springdemo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class CustomerRestController {
+
+    // autowire the CustomerService
+    @Autowired
+    private CustomerService customerService;
+
+    // add  mapping for GET /customers
+
 }
